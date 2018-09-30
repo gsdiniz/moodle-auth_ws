7168eff6f55f413c1e2a42772e9169762ddf9e50
Daniel Neis Araujo <danielneis@gmail.com>
2018-09-30 16:07:04 -0300

    criando alunos a partir do sponte

---
+++ README-sponte.md
@@ -0,0 +1,9 @@
+SPONTE Educacional integration with Moodle authentication
+
+This plugins uses functions available at:
+
+http://api.sponteeducacional.net.br/WSAPIEdu.asmx
+
+http://api.sponteeducacional.net.br/WSAPIEdu.asmx?op=GetAlunos
+
+ValidaLoginPortal
--- auth.php
+++ auth.php
@@ -82,6 +82,88 @@ class auth_plugin_ws extends auth_plugin_base {
      * @return int 0 means success, 1 means failure
      */
     public function sync_users(progress_trace $trace, $doupdates = false) {
+        global $DB;
+
+        $functionname = 'GetAlunos';
+        $params = array('sParametrosBusca' => 'Inadimplente=0');
+        $dbman = $DB->get_manager();
+        $table = new xmldb_table('tmp_extuser');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('username', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('firstname', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('email', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_index('username', XMLDB_INDEX_UNIQUE, array('username'));
+
+        print_string('creatingtemptable', 'auth_ws', 'tmp_extuser');
+        $dbman->create_temp_table($table);
+
+        print_string('fetchingstudents', 'auth_ws');
+        $result = $this->call_ws($this->config->serverurl, $functionname, $params);
+
+        print_string('savingtotemptable', 'auth_ws');
+        foreach ($result->GetAlunosResult->wsAluno as $user) {
+            $newuser = array('username'=> $user->LoginPortal,
+                             'firstname' => $user->Nome, 'email' => $user->Email);
+            $DB->insert_record_raw('tmp_extuser', $newuser, false);
+        }
+
+        /// preserve our user database
+        /// if the temp table is empty, it probably means that something went wrong, exit
+        /// so as to avoid mass deletion of users; which is hard to undo
+        $count = $DB->count_records_sql('SELECT COUNT(*) AS count, 1 FROM {tmp_extuser}');
+        if ($count < 1) {
+            print_string('didntgetusersfromws', 'auth_ws');
+            exit;
+        } else {
+            print_string('gotcountrecordsfromws', 'auth_ws', $count);
+        }
+
+        $sql = 'SELECT e.*
+                  FROM {tmp_extuser} e
+             LEFT JOIN {user} u
+                    ON (e.username = u.username)
+                 WHERE u.id IS NULL';
+        $add_users = $DB->get_records_sql($sql);
+        if (!empty($add_users)) {
+            foreach ($add_users as $user) {
+                $user->confirmed = 1;
+                $user->auth = $this->authtype;
+                $id = user_create_user($user, false);
+                echo 'creted user: '.$user->firstname.'<br/>';
+            }
+        }
+
+        echo 'updating users...<br/>';
+        $sql = 'UPDATE {user} u
+                  JOIN {tmp_extuser} e
+                    ON (e.username = u.username)
+                   SET u.firstname = e.firstname,
+                       u.email = e.email,
+                       u.suspended = 0,
+                       u.deleted = 0,
+                       u.confirmed = 1
+                 WHERE u.auth = "ws"';
+        echo 'users updated.<br/>';
+
+        $sql = 'SELECT u.id,u.username
+                  FROM {user} u
+             LEFT JOIN {tmp_extuser} e
+                    ON (e.username = u.username)
+                 WHERE e.id IS NULL
+                   AND u.auth = "ws"
+                   AND u.deleted = 0
+                   AND u.suspended = 0';
+        $remove_users = $DB->get_records_sql($sql);
+        foreach ($remove_users as $user) {
+            $updateuser = new stdClass();
+            $updateuser->id = $user->id;
+            $updateuser->suspended = 1;
+            user_update_user($updateuser, false);
+            echo print_string('suspenduser', 'auth_db', array('name'=>$user->username, 'id'=>$user->id));
+            echo "<br/>";
+            \core\session\manager::kill_user_sessions($user->id);
+        }
         return true;
     }
 
